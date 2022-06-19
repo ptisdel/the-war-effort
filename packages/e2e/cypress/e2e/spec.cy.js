2e7a46f25f9c835e10de04aa4e642340cf307dc6
Patrick Tisdel <patrick.tisdel@pmcorp.corp>
2022-06-19 13:46:39 -0400

    test: 💍 add cypress e2e command

--- packages/e2e/cypress/e2e/spec.cy.js
+++ packages/e2e/cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Client works as expected', () => {
+  it('visits the client site', () => {
+    cy.visit(Cypress.env('CLIENT_URL'));
+  });
+});
