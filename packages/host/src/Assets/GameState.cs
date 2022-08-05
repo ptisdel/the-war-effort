1fd308e630a116c268e3587915570a45b40c5d1d
Patrick Tisdel <patrick.tisdel@pmcorp.corp>
2022-08-04 20:20:26 -0400

    fix: 🐛 add missing region connection

--- packages/host/src/Assets/GameState.cs
+++ packages/host/src/Assets/GameState.cs
@@ -24,7 +24,7 @@ public class GameState : MonoBehaviour
 
     private List<Region> _defaultRegions = new List<Region>() {
         new Region("0", "A", new Point(-1, 2), new List<string>() { "1", "2" }),
-        new Region("1", "B", new Point(1, 4), new List<string>() { "0" }),
+        new Region("1", "B", new Point(1, 4), new List<string>() { "0", "3" }),
         new Region("2", "C", new Point(-5, 1), new List<string>() { "0", "3" }),
         new Region("3", "D", new Point(0, 0), new List<string>() { "1", "2" }),
     };
